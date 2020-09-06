935679d7500f161c87087aedee4962965ccccfc3
mingxing <624866694@qq.com>
2020-09-06 13:49:50 +0800

    init test code project

---
+++ src/main/java/org/others/LFU.java
@@ -0,0 +1,14 @@
+package org.others;
+
+public class LFU {
+
+    public static void main(String[] args) {
+        int[][] input = {{1,1,1},{1,2,2},{1,3,2},{1,2,4},{1,3,5},{2,2},{1,4,4},{2,1}};
+
+    }
+
+    public int[] LFU (int[][] operators, int k) {
+        // write code here
+        return null;
+    }
+}
